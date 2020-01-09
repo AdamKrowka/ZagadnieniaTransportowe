0a841d471656270fd7699a0a0187ee46e763f163
Adam96 <adamkrowka108@gmail.com>
2020-01-09 14:49:43 +0100

    Updates

--- precache-manifest.7e90f1fbe85a87468624b541e7812296.js
+++ precache-manifest.7e90f1fbe85a87468624b541e7812296.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "47f320e558c9f1e301e91da0bc1ccca3",
+    "revision": "a6921c3b2a998695c4e265da6525e0e0",
     "url": "/ZagadnieniaTransportowe/index.html"
   },
   {
-    "revision": "498fdbc0e7c05531d2d4",
+    "revision": "41b2ec8d9588a9fe4f53",
     "url": "/ZagadnieniaTransportowe/static/css/main.44c06c4f.chunk.css"
   },
   {
-    "revision": "f489ffee3cfb7fdb2d51",
-    "url": "/ZagadnieniaTransportowe/static/js/2.d1ef5248.chunk.js"
+    "revision": "637a8bf3e481db48e5f1",
+    "url": "/ZagadnieniaTransportowe/static/js/2.599ee11f.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/ZagadnieniaTransportowe/static/js/2.d1ef5248.chunk.js.LICENSE"
+    "url": "/ZagadnieniaTransportowe/static/js/2.599ee11f.chunk.js.LICENSE"
   },
   {
-    "revision": "498fdbc0e7c05531d2d4",
-    "url": "/ZagadnieniaTransportowe/static/js/main.d1dbd83c.chunk.js"
+    "revision": "41b2ec8d9588a9fe4f53",
+    "url": "/ZagadnieniaTransportowe/static/js/main.6be09ac5.chunk.js"
   },
   {
     "revision": "9dfafe7073256362ac37",
