7d7a2226fac4f432cdabc4015b391b1831378884
Adam96 <adamkrowka108@gmail.com>
2020-01-09 13:56:24 +0100

    Updates

---
+++ precache-manifest.73864475b027f113f9d4df49bad14a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733fed3757d705fe57b52ed358cf68db",
+    "url": "/ZagadnieniaTransportowe/index.html"
+  },
+  {
+    "revision": "a306f917126a8a96a84e",
+    "url": "/ZagadnieniaTransportowe/static/css/main.91a7f996.chunk.css"
+  },
+  {
+    "revision": "637a8bf3e481db48e5f1",
+    "url": "/ZagadnieniaTransportowe/static/js/2.599ee11f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ZagadnieniaTransportowe/static/js/2.599ee11f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a306f917126a8a96a84e",
+    "url": "/ZagadnieniaTransportowe/static/js/main.d2ba0b39.chunk.js"
+  },
+  {
+    "revision": "9dfafe7073256362ac37",
+    "url": "/ZagadnieniaTransportowe/static/js/runtime-main.cabe3ebd.js"
+  }
+]);
